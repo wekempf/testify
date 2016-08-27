8c0e8854c370013f7d578563e1aae602195f8aa2
Bill Kempf <bkempf@rightrez.com>
2016-08-27 09:24:14 -0400

    Add populate functionality.
    Add the Populate extension method and overloads to Any to allow for the
    population of all public properties with anonymous values. Support both
    deep and shallow population.

--- src/Testify/AnonymousDataExtensions.cs
+++ src/Testify/AnonymousDataExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 namespace Testify
@@ -22,6 +24,42 @@ namespace Testify
             Argument.NotNull(anon, nameof(anon));
 
             return (T)anon.Any(typeof(T));
+        }
+
+        /// <summary>
+        /// Creates an object of the specified type and optionally populates the properties.
+        /// </summary>
+        /// <param name="anon">The anonymous data provider to use.</param>
+        /// <param name="type">The type to create..</param>
+        /// <param name="populateOption">Specifies how to populate the properties.</param>
+        /// <returns>An instance of the specified type.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="anon"/> or <paramref name="type"/>
+        /// is null.</exception>
+        public static object Any(this IAnonymousData anon, Type type, PopulateOption populateOption)
+        {
+            Argument.NotNull(anon, nameof(anon));
+            var instance = anon.Any(type);
+            if (populateOption != PopulateOption.None)
+            {
+                anon.Populate(instance, populateOption == PopulateOption.Deep);
+            }
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Anies the specified populate option.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to create.</typeparam>
+        /// <param name="anon">The anonymous data provider to use.</param>
+        /// <param name="populateOption">Specifies how to populate the properties.</param>
+        /// <returns>An instance of the specified type.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="anon"/> is null.</exception>
+        public static T Any<T>(this IAnonymousData anon, PopulateOption populateOption)
+        {
+            Argument.NotNull(anon, nameof(anon));
+
+            return (T)anon.Any(typeof(T), populateOption);
         }
 
         /// <summary>
@@ -55,6 +93,62 @@ namespace Testify
             Argument.NotNull(anon, nameof(anon));
 
             anon.Freeze(typeof(T), value);
+        }
+
+        /// <summary>
+        /// Populates the specified instance by assigning all properties to anonymous values.
+        /// </summary>
+        /// <param name="anon">The anonymous data provider to use.</param>
+        /// <param name="instance">The instance to populate.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="anon"/> is null.</exception>
+        public static void Populate(this IAnonymousData anon, object instance)
+        {
+            Argument.NotNull(anon, nameof(anon));
+
+            anon.Populate(instance, true);
+        }
+
+        /// <summary>
+        /// Populates the specified instance by assigning all properties to anonymous values.
+        /// </summary>
+        /// <param name="anon">The anonymous data provider to use.</param>
+        /// <param name="instance">The instance to populate.</param>
+        /// <param name="deep">If set to <see langword="true"/> then properties are assigned recursively, populating
+        /// the entire object tree.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="anon"/> is null.</exception>
+        public static void Populate(this IAnonymousData anon, object instance, bool deep)
+        {
+            Argument.NotNull(anon, nameof(anon));
+
+            var queue = deep ? new Queue<object>() : null;
+            var current = instance;
+            while (true)
+            {
+                if (current != null)
+                {
+                    var type = current.GetType();
+                    var properties =
+                        from prop in type.GetRuntimeProperties()
+                        where prop.CanWrite && prop.SetMethod.IsPublic
+                        select prop;
+                    foreach (var prop in properties)
+                    {
+                        var value = anon.Any(prop.PropertyType);
+                        prop.SetValue(current, value);
+                        if (deep)
+                        {
+                            queue.Enqueue(value);
+                        }
+                    }
+                }
+
+                if (!deep || !queue.Any())
+                {
+                    break;
+                }
+
+                current = queue.Dequeue();
+            }
         }
 
         /// <summary>
